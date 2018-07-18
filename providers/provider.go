6206f2a1f874a009b901ba4421b6245c1f5f3713
James Bardin <j.bardin@gmail.com>
2018-07-17 21:07:34 -0400

    add Config to the PlanResourceChangeRequest
    In order for providers to determine if a computed value was unset in the
    configuration, the configuration must be passed along with the prior and
    proposed states. Terraform core will still handle the creation of the
    ProposedNewState to ensure correctness, but the raw configuration value
    will also be supplied for comparison.

--- providers/provider.go
+++ providers/provider.go
@@ -187,6 +187,11 @@ type PlanResourceChangeRequest struct {
 	// changes required to create the proposed final state.
 	ProposedNewState cty.Value
 
+	// Config is the resource configuration, before being merged
+	// with the PriorState. Config is supplied for reference, but Provider
+	// implementations should prefer the ProposedNewState in most circumstances.
+	Config cty.Value
+
 	// PriorPrivate is the previously saved private data returned from the
 	// provider during the last apply.
 	PriorPrivate []byte
